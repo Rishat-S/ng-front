9dbfb4879a139c111aed0548161a5b373b72dd59
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-01 22:30:43 +0300

    services edited and helpers

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snackbar: MatSnackBar) { }
+
+  public showSnackBar(massage: string): void {
+    // @ts-ignore
+    this.snackbar.open(massage, null, {
+      duration: 2000
+    });
+  }
+
 }
